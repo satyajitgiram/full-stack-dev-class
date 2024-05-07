8405909ee0d1aabfb4c62eadd95f4684a765227a
satyajitgiram <satyajitgiram2000@gmail.com>
2024-05-07 16:21:41 +0530

    if-else, switch case, loops

--- script.js
+++ script.js
@@ -4,3 +4,41 @@
 
 
 console.log("This file is running")
+
+
+// let mark =58;
+
+// if(mark >= 90) {
+//     console.log('A')
+// }
+// else if (mark >= 80){
+//     console.log("B")
+// }
+// else if (mark >= 70){
+//     console.log("C")
+// }
+// else{
+//     console.log('D')
+// }
+
+
+
+// let num = 2
+// switch(num){
+//     case 1:console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     default : console.log("Default is running");
+//     break;
+// }
+
+
+for(let i=1; i<=10; i+=1){
+    console.log(19*i)
+}
+
+
+
+//
