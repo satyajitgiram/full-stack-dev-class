0396b950ebe5dd8aa74012b30b5cfed8ff106265
satyajitgiram <satyajitgiram2000@gmail.com>
2024-05-10 14:50:20 +0530

    learned objects & strings

--- script.js
+++ script.js
@@ -62,19 +62,80 @@ console.log("This file is running")
 
 // OBJECT
 
-let car = {
-    "brand":"BMW",
-    "model": "X5",
-    "color": "Mat Black",
-    "type": "Sport",
+// let car = {
+//     "brand":"BMW",
+//     "model": "X5",
+//     "color": "Mat Black",
+//     "type": "Sport",
 
-    status: function(){
-        console.log("Car's is running ..... ")
+//     status: function(){
+//         console.log("Car's is running ..... ")
+//     }
+
+// }
+
+
+const person = {
+
+    firstName : "Satyajit",
+    lastName : "Giram",
+    id : "55555",
+
+    fullName : function(){
+        return this.firstName + " " + this.lastName
     }
+
+
 
 }
 
-car.status()
+// let fullname = person.fullName()
+
+// console.log("This is full name =>",fullname)
+
+
+
+
+// car.status()
+
+
+
+// let moNo = 9595959959
+let name = "My name is john"
+
+
+console.log('this\\this')
+
+// \b - backspace
+// \f - form feed
+// \t - tab
+
+
+// let poem = 'Johny Johny Yes Papa Nursery Rhyme
+
+// Johny Johny yes papa.
+// Eating sugar no papa.
+// Telling lies no papa.
+// Open your mouth.
+// Ha!
+// Ha!
+// Ha!'
+
+
+// let mobile = "93939393"
+// let newMobile = new String("93939393")
+// let oldMobile = new String("93939393")
+// console.log(typeof newMobile)
+
+
+let text = "Hello World!"
+console.log(text.charAt(6))
+
+
+
+
+
+console.log(name.length)
 
 
 //
